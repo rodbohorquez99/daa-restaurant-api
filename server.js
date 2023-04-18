188c747489376d0fcd1079197a197322e0ee5ad8
Rodrigo Bohorquez <114632183+rodbohorquez99@users.noreply.github.com>
2023-04-18 14:34:09 -0600

    conexion con mongo

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('dotenv').config();
-
 
 const homeRoutes = require('./src/routes/home.routes');
 const reservationsRoutes = require('./src/routes/reservations.routes');
@@ -11,4 +11,15 @@ app.use('/', homeRoutes);
 app.use('/reservations', reservationsRoutes);
 app.use('/users', usersRoutes);
 
-app.listen(process.env.PORT, () => console.log('Server listening at port ' +process.env.PORT));
+const main = async () => {
+    try{
+        await mongoose.connect(process.env.DB_URL);
+        app.listen(process.env.PORT, () => console.log('Server listening at port ' +process.env.PORT));
+
+    } catch(error){
+        console.error(error);
+        process.exit(1);
+    }
+ };
+
+main();
