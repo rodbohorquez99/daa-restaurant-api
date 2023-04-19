5a6b05306687946146d1d2e7349b1a2c81e4f48d
Rodrigo Bohorquez <114632183+rodbohorquez99@users.noreply.github.com>
2023-04-18 20:28:55 -0600

    guardar  password

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,7 +1,46 @@
-const getAllUsers = (req, res) => res.status(200).send('GET /users succeded');
+const bcrypt = require('bcrypt');
 
-const createUser = (req, res) => res.status(201).send('POST /users succeded');
+const User = require('../models/user.model');
 
-const deleteUser = (req, res) => res.status(204).send();
+const getAllUsers = async (req, res, next) => {
+    try{
+        const allUsers = await User.find().select('-password -token');
+        return res.status(200).json(allUsers);
+    }catch (error){
+        return next(error);
+     }
+};
 
-module.exports = { getAllUsers, createUser, deleteUser }
+const createUser = async (req, res, next)=>{
+    try{
+        const {username, name, password} = req.body;
+        const encryptedPassword = await bcrypt.hash(password,10);
+
+        const newUser = new User({
+            username: username.toLowerCase(),
+            name,
+            password: encryptedPassword
+        });
+
+        const insertedUser = await newUser.save();
+        const returnUser = insertedUser.toObject();
+        delete returnUser.password;
+        return res.status(201).json(returnUser);
+    }catch(error){
+        return next(error);
+    }
+};
+
+
+const deleteUser =async   (req, res, next )=>{
+try{
+    const id=req.params.id;
+    await User.findByIdAndDelete(id);
+    return res.status(204).send();
+}catch(error){
+    return next(error);
+}
+};
+ 
+
+module.exports = {getAllUsers, createUser, deleteUser};
